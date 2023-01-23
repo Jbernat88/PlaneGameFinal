2ceeb65897ac8dfeebc10dedf830edf396f57733
Jbernat88 <91455587+Jbernat88@users.noreply.github.com>
2023-01-23 12:06:11 +0100

    prrimer

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Cinemachine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform playerModel;
+
+
+    [Space]
+
+    [Header("Parameters")]
+    public float xySpeed = 18;
+    public float lookSpeed = 340;
+    public float forwardSpeed = 6;
+
+    [Space]
+
+    [Header("Public References")]
+    public Transform aimTarget;
+    public CinemachineDollyCart dolly;
+    public Transform cameraParent;
+
+    [Space]
+
+    [Header("Particles")]
+    public ParticleSystem trail;
+    public ParticleSystem circle;
+    public ParticleSystem barrel;
+    public ParticleSystem stars;
+
+    void Start()
+    {
+        playerModel = transform.GetChild(0);
+        SetSpeed(forwardSpeed);
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        LocalMove(h, v, xySpeed);
+        RotationLook(h,v, lookSpeed);
+        HorizontalLean(playerModel, h, 80, .1f);
+
+        //Controles del jugador
+        if (Input.GetKeyDown(KeyCode.C))
+            Boost(true);
+
+        if (Input.GetKeyDown(KeyCode.C))
+            Boost(false);
+        
+        if (Input.GetKeyDown(KeyCode.X))
+            Break(true);
+
+        if (Input.GetKeyDown(KeyCode.X))
+            Break(false);
+
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E))
+        {
+            int dir = Input.GetKeyDown(KeyCode.Q) ? -1 : 1;
+            QuickSpin(dir);
+        }
+
+        
+    }
+
+    //Cambiamos el movimento a local para no afectar a la camara
+    void LocalMove(float x, float y, float speed)
+    {
+        transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+        ClampPosition();
+    }
+
+    //Limites Camara
+    void ClampPosition()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    //Mira el objeto aimPosition
+    void RotationLook(float h, float v, float speed)
+    {
+        aimTarget.parent.position = Vector3.zero;
+        aimTarget.localPosition = new Vector3(h, v, 1);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed * Time.deltaTime);
+    }
+
+    //Para mejorar la sensacion de movimiento nos input z.
+    void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)
+    {
+        Vector3 targetEulerAngels = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
+    }
+
+    //Dibuja el objeto aimPosition 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(aimTarget.position, .5f);
+        Gizmos.DrawSphere(aimTarget.position, .15f);
+
+    }
+
+    public void QuickSpin(int dir)
+    {
+        if (!DOTween.IsTweening(playerModel))
+        {
+            //Hace que el player rote rapidamente.
+            playerModel.DOLocalRotate(new Vector3(playerModel.localEulerAngles.x, playerModel.localEulerAngles.y, 360 * -dir), .4f, RotateMode.LocalAxisAdd).SetEase(Ease.OutSine);
+            barrel.Play();
+        }
+    }
+
+    //Mantiene la velocidad de forward speed
+    void SetSpeed(float x)
+    {
+        dolly.m_Speed = x;
+    }
+
+    //Zoom de la camara cuando hacemos el boost.
+    void SetCameraZoom(float zoom, float duration)
+    {
+        cameraParent.DOLocalMove(new Vector3(0, 0, zoom), duration);
+    }
+
+    
+    void DistortionAmount(float x)
+    {
+        Camera.main.GetComponent<PostProcessVolume>().profile.GetSetting<LensDistortion>().intensity.value = x;
+    }
+
+    void FieldOfView(float fov)
+    {
+        cameraParent.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens.FieldOfView = fov;
+    }
+
+    void Chromatic(float x)
+    {
+        Camera.main.GetComponent<PostProcessVolume>().profile.GetSetting<ChromaticAberration>().intensity.value = x;
+    }
+    
+
+    void Boost(bool state)
+    {
+
+        if (state)
+        {
+            cameraParent.GetComponentInChildren<CinemachineImpulseSource>().GenerateImpulse();
+            trail.Play();
+            circle.Play();
+        }
+        else
+        {
+            trail.Stop();
+            circle.Stop();
+        }
+        trail.GetComponent<TrailRenderer>().emitting = state;
+
+        float origFov = state ? 40 : 55;
+        float endFov = state ? 55 : 40;
+        float origChrom = state ? 0 : 1;
+        float endChrom = state ? 1 : 0;
+        float origDistortion = state ? 0 : -30;
+        float endDistorton = state ? -30 : 0;
+        float starsVel = state ? -20 : -1;
+        float speed = state ? forwardSpeed * 2 : forwardSpeed;
+        float zoom = state ? -7 : 0;
+
+        DOVirtual.Float(origChrom, endChrom, .5f, Chromatic);
+        DOVirtual.Float(origFov, endFov, .5f, FieldOfView);
+        DOVirtual.Float(origDistortion, endDistorton, .5f, DistortionAmount);
+        var pvel = stars.velocityOverLifetime;
+        pvel.z = starsVel;
+
+        DOVirtual.Float(dolly.m_Speed, speed, .15f, SetSpeed);
+        SetCameraZoom(zoom, .4f);
+    }
+
+    //Frenado despues de instanciar el boost
+    void Break(bool state)
+    {
+        float speed = state ? forwardSpeed / 3 : forwardSpeed;
+        float zoom = state ? 3 : 0;
+
+        DOVirtual.Float(dolly.m_Speed, speed, .15f, SetSpeed);
+        SetCameraZoom(zoom, .4f);
+    }
+}
