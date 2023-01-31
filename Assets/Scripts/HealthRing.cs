0b25fe6b9f07a8d8fdbc938586b42ac9c3c44530
Jbernat88 <91455587+Jbernat88@users.noreply.github.com>
2023-01-31 11:22:42 +0100

    mmm

---
+++ Assets/Scripts/HealthRing.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthRing : MonoBehaviour
+{
+    public TextMeshProUGUI healthText;
+    public Image  ringHealthBar;
+   
+
+    private float maxHealth; //Vida Max
+    public float health;
+    float lerpSpeed;
+
+    public PlayerController playerControllerScript;
+
+    private void Start()
+    {
+        playerControllerScript = FindObjectOfType<PlayerController>();
+        maxHealth = health;
+    }
+
+    private void Update()
+    {
+        healthText.text = $"{health}%";
+        if (health > maxHealth) health = maxHealth; //sI SUMO VIDA ME EL MAXIMO SERA SIENDO 100
+
+        lerpSpeed = 3f * Time.deltaTime; //VELOCIDAD DE BAJAR LA VIDA
+
+        if( health <= 0)
+        {
+             
+        }
+
+        HealthBarFiller();
+        ColorChanger();
+    }
+
+    void HealthBarFiller()
+    {
+        //velocidad a la que bajara de un punto de vida hasat el proximo.
+        ringHealthBar.fillAmount = Mathf.Lerp(ringHealthBar.fillAmount, (health / maxHealth), lerpSpeed);
+    }
+    void ColorChanger()
+    {
+        Color healthColor = Color.Lerp(Color.red, Color.white, (health / maxHealth));      
+        ringHealthBar.color = healthColor;
+    }
+    public void Damage(float damagePoints)
+    {
+        if (health != 0)
+            health -= damagePoints;
+    }
+    public void Heal(float healingPoints)
+    {
+        if (health < maxHealth)
+            health += healingPoints;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public TextMeshProUGUI pointText;
+    private int score = 0;
+    private int maxScore = 3;
+    public int points;
+
+    private int currentHealth = 100;
+    public HealthRing healthRingScript;
+
+    private void Awake()
+    {
+        healthRingScript = FindObjectOfType<HealthRing>();
+        healthRingScript.health = currentHealth; //Metemos la vida en la variable Health.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointText.text = $"Coins: {score}/{maxScore}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            healthRingScript.Damage(10);
+        }
+    }
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+
+        if (otherCollider.gameObject.CompareTag("Money")) //Moneda
+        {
+            Destroy(otherCollider.gameObject);
+                           
+            UpdateScore(points); //Permite sumar los puntos de cada objeto          
+        }
+
+        if (otherCollider.gameObject.CompareTag("Bullet"))
+        {
+            UpdateLive(-10);
+        }
+    }
+
+    public void UpdateScore(int pointsToAdd)
+    {
+        score++;//Linea per actualitzar el score
+        pointText.text = $"Coins: {score}/{maxScore}";
+
+       // Debug.Log($"Tienes");
+    }
+
+    private void UpdateLive(int Change)
+    {
+        currentHealth += Change;
+        healthRingScript.health = currentHealth;
+    }
+}
