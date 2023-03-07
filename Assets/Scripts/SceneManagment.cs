8e29895e828a6e69d0c606e86a257b80f3c5ee92
Joan Bernat <joanbernatmunyoz@gmail.com>
2023-03-08 00:17:30 +0100

    sgffv

--- Assets/Scripts/SceneManagment.cs
+++ Assets/Scripts/SceneManagment.cs
@@ -5,21 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagment : MonoBehaviour
 {
+    public AudioSource button;
     //Load the levels scene
     public void Levels()
     {
         SceneManager.LoadScene("Levels");
+
+        button.Play();
     }
 
     //Load the menu scene
     public void Menu()
     {
         SceneManager.LoadScene("Menu");
+
+        button.Play();
     }
 
     //Load the options scene
     public void Options()
     {
         SceneManager.LoadScene("Options");
+
+        button.Play();
     }
 }
